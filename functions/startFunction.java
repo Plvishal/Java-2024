c81117eca0f31f8671afc4cde09a037519a33a76
Vishal <vishalpal1501@gmail.com>
2024-02-07 17:57:22 +0530

    function with parameter and arguments

--- functions/startFunction.java
+++ functions/startFunction.java
@@ -1,6 +1,14 @@
 public class startFunction{
+    public static int helloWorld(int num1,int num2){
+        int sum=num1+num2;
+        return sum;
+
+    }
     public static void main(String args[]){
-        System.out.println("Hello");
+        int a=20;
+        int b=20;
+        int sum= helloWorld(a,b);
+        System.out.println(sum);
 
     }
 }
