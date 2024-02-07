5b013786e7f99d277bff45a482cb391c639ba48f
Vishal <vishalpal1501@gmail.com>
2024-02-07 20:36:34 +0530

    product of two number  using the function

---
+++ functions/product.java
@@ -0,0 +1,14 @@
+public class product{
+    public static int multiplier(int a , int b){
+        int mul= a*b;
+        return mul;
+    }
+    public static void main(String args[]){
+        int a=10;
+        int b=7;
+       int mul= multiplier(a,b);
+       System.out.println(mul);
+
+    }
+    
+}
