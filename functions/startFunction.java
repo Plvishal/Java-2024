f4581c40b14da670e54ea2642cc24c9d2bfaa55a
Vishal <vishalpal1501@gmail.com>
2024-02-07 20:25:23 +0530

    write swap function for the two varibales

--- functions/startFunction.java
+++ functions/startFunction.java
@@ -4,11 +4,18 @@ public class startFunction{
         return sum;
 
     }
+    public static void swap(int a, int b){
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a swap value"+ a);
+        System.out.println("b swap value" +b);
+    }
     public static void main(String args[]){
         int a=20;
-        int b=20;
-        int sum= helloWorld(a,b);
-        System.out.println(sum);
+        int b=2;
+        swap(a,b);
+      
 
     }
 }
