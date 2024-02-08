f2d9016c77f85aa4ae0e5d39bd2d38fc8ff1d983
Vishal <vishalpal1501@gmail.com>
2024-02-08 18:18:51 +0530

    Write a function to check given numbe  is palindrome or not

---
+++ functions/practice/palindromeNumber.java
@@ -0,0 +1,33 @@
+package practice;
+
+import java.util.Scanner;
+
+public class palindromeNumber {
+    public static boolean isPalindrome(int number){
+        int palindrome= number;
+        int reverse= 0;
+        while (palindrome !=0) {
+            int remainder= palindrome%10;
+            reverse= reverse*10+remainder;
+            palindrome= palindrome/10;
+           
+        }
+
+        if(number==reverse){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int num;
+        System.out.print("Enter the number  = ");
+        num= sc.nextInt();
+        if(isPalindrome(num)){
+            System.out.println("Number  is Palindrome");
+        }else{
+            System.out.println("Number is Not Palindrome");
+        }
+    }
+    
+}
