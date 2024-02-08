c52fe9062e9d57a390141de34ca9f124306fced1
Vishal <vishalpal1501@gmail.com>
2024-02-08 15:39:38 +0530

    optimize code for prime number

--- functions/primeNumber.java
+++ functions/primeNumber.java
@@ -1,19 +1,16 @@
 public class primeNumber {
 // Write a function to chech the number is prime or not 
-    public static boolean isPrime(int n){
-        boolean isPrime=true;
+    public static boolean isPrime(int n){       
         for(int i=2;i<=n-1;i++){
             if(n % i == 0){
-
-                isPrime= false;
-                break;
+                return false;               
             }
-        }
-        return isPrime;
+        } 
+        return true;      
     }
     public static void main(String args[]){
 
-        System.out.println("Number is Prime =  " + isPrime(12));
+        System.out.println("Number is Prime =  " + isPrime(17));
 
     }
 }
