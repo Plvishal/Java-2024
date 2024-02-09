a7b7504fed8880286aed989143a4f0ae46874048
Vishal <vishalpal1501@gmail.com>
2024-02-09 14:35:03 +0530

    Array creations

---
+++ Array/arrayOperation.java
@@ -0,0 +1,21 @@
+package Array;
+import java.util.*;
+
+public class arrayOperation {
+    public static void main(String args[]){
+        Scanner sc= new Scanner (System.in);
+        int marks[]= new int[10];
+        
+         System.out.print("Enter Physics marks= ");
+         marks[0]=sc.nextInt();  
+         System.out.print("Enter Chemistry marks= ");
+         marks[1]=sc.nextInt();  
+         System.out.print("Enter Maths marks= ");
+         marks[2]=sc.nextInt();  
+         System.out.println("Pyhics: " + marks[0]);
+         System.out.println("Chemistry: " + marks[1]);
+         System.out.println("Maths: " + marks[2]);
+
+          }
+    
+}
