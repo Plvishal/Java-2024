fdc4da2e6d898bb6d708590cff4f99606880c591
Vishal <vishalpal1501@gmail.com>
2024-02-15 18:30:38 +0530

    Multi level inheritamce

--- OPP's/InheritanceConcept.java
+++ OPP's/InheritanceConcept.java
@@ -1,16 +1,39 @@
 
 public class InheritanceConcept {
     public static void main(String[] args) {
-        Fish f = new Fish();
-        f.eat();
-        f.swim();
-
+        Dogs d = new Dogs();
+        d.color = "Blacks";
+        d.eat();
+        d.legs = 4;
+        d.name = "German Shefert";
+        System.out.println("Dog Name : " + d.name);
+        System.out.println("Color : " + d.color);
+        System.out.println("Legs : " + d.legs);
     }
 
 }
 
 // Base Class
+// class Animal {
+// void eat() {
+// System.out.println("Eat");
+// }
+
+// void breathe() {
+// System.out.println("Breathe");
+// }
+// }
+
+// class Fish extends Animal {
+// // int fins;
+// void swim() {
+// System.out.println("Swim in water");
+// }
+// }
+
 class Animal {
+    String color;
+
     void eat() {
         System.out.println("Eat");
     }
@@ -20,9 +43,10 @@ class Animal {
     }
 }
 
-class Fish extends Animal {
-    // int fins;
-    void swim() {
-        System.out.println("Swim in water");
-    }
+class Mammal extends Animal {
+    int legs;
+}
+
+class Dogs extends Mammal {
+    String name;
 }
