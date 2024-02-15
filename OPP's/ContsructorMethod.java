c2814bf3bf66afba3deac03a8c8802e15162462b
Vishal <vishalpal1501@gmail.com>
2024-02-15 17:34:12 +0530

    In constructor method using the parameter

--- OPP's/ContsructorMethod.java
+++ OPP's/ContsructorMethod.java
@@ -1,17 +1,24 @@
 
 public class ContsructorMethod {
     public static void main(String args[]) {
-        Student s = new Student();
-        // System.out.println(s);
+        Student s = new Student("Vishal", 23);
+        System.out.println("Name : " + s.name);
+        System.out.println("Age : " + s.age);
     }
 }
 
 class Student {
-    String name = "Vishal Pal";
-    int age = 98;
+    String name;
+    int age;
 
-    Student() {
-        System.out.println("Name : " + this.name + "   Age : " + this.age);
-
+    /*
+     * Student() {
+     * System.out.println("Name : " + this.name + "   Age : " + this.age);
+     * 
+     * }
+     */
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
