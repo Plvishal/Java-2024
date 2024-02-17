6884afdd90741f3258d280d0445249f781cabd04
Vishal <vishalpal1501@gmail.com>
2024-02-17 23:30:33 +0530

    Insertion sort :Accending Order

---
+++ SortingAlgorithm/InsertionSort.java
@@ -0,0 +1,33 @@
+package SortingAlgorithm;
+
+public class InsertionSort {
+
+    // Accending Order
+    public static void insertionSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            // Insertion
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void pirntArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 2, 5, 3, 1 };
+        insertionSort(arr);
+        pirntArr(arr);
+
+    }
+}
