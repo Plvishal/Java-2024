496d97011e01414bb0d0181bf23889269dbbf3b3
Vishal <vishalpal1501@gmail.com>
2024-02-17 22:29:24 +0530

    Bubble sort

---
+++ SortingAlgorithm/bubbleSort.java
@@ -0,0 +1,30 @@
+package SortingAlgorithm;
+
+public class bubbleSort {
+    // Increasing order
+    public static void bubbleAcendingOrder(int arr[]) {
+        for (int turn = 0; turn < arr.length; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 2, 7, 1 };
+        bubbleAcendingOrder(arr);
+        printArr(arr);
+    }
+}
