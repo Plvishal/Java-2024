cd09e1a03ca2399be7471f543888f157b2d1c1e9
Vishal <vishalpal1501@gmail.com>
2024-02-19 15:34:17 +0530

    How to define the string

---
+++ StringConcept/DefineTheString.java
@@ -0,0 +1,12 @@
+package StringConcept;
+
+public class DefineTheString {
+    public static void main(String[] args) {
+        char arr[] = { 'a', 'b', 'd' };
+        String str = "dsfgyugfewe";
+        String str1 = new String("dsfr@dgh");
+        System.out.println(arr);
+        System.out.println(str);
+        System.out.println(str1);
+    }
+}
