ce891963433d102c2b73fd2732192d5a50fd0df3
Vishal <vishalpal1501@gmail.com>
2024-02-19 13:08:09 +0530

    Search elemtn in the matrix using a key

--- 2DArray/TwoArrayCreation.java
+++ 2DArray/TwoArrayCreation.java
@@ -1,9 +1,25 @@
 import java.util.Scanner;
 
 public class TwoArrayCreation {
+    public static boolean searchMatrixByKey(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+
+                    System.out.print("Key Found at  : " + "(" + i + "," + j + ")");
+                    return true;
+                }
+            }
+
+        }
+        System.out.println("Key not found");
+        return false;
+    }
+
     public static void main(String[] args) {
 
         int matrix[][] = new int[3][3];
+        int key = 5;
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
@@ -12,10 +28,15 @@ public class TwoArrayCreation {
         }
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
+
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
+
         }
+
+        searchMatrixByKey(matrix, key);
+
     }
 
 }
