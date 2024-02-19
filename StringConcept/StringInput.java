69789581d15552c261d5cfc4c59e866a335387c5
Vishal <vishalpal1501@gmail.com>
2024-02-19 15:39:05 +0530

    Take input as String from a user

---
+++ StringConcept/StringInput.java
@@ -0,0 +1,13 @@
+package StringConcept;
+
+import java.util.*;
+
+public class StringInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        // String anyThing = sc.next();
+        String anyThing = sc.nextLine();
+        System.out.println(anyThing);
+    }
+}
