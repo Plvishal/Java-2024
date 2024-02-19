8519dc1d9b60347b05ff6f9a37ac50b765e56298
Vishal <vishalpal1501@gmail.com>
2024-02-19 15:52:40 +0530

    Concatenation and finding the length of the string

---
+++ StringConcept/ConcatenateAndLenght.java
@@ -0,0 +1,13 @@
+package StringConcept;
+
+public class ConcatenateAndLenght {
+    public static void main(String[] args) {
+        String str = "Ramayan gatha";
+        String firstName = "Vishal";
+        String lastName = "Pal";
+        // For finding the length of the String
+        System.out.println(str.length());
+        // Concatenate multiple string
+        System.out.println(firstName + " " + lastName);
+    }
+}
