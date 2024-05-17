7fc37c1c700f8e2bcabefb05144dc7a08b7a6ce0
Vishal <vishalpal1501@gmail.com>
2024-05-17 17:48:23 +0530

    Sum of natural number using the while loop

--- Recursion/Iteration.java
+++ Recursion/Iteration.java
@@ -1,19 +1,12 @@
 public class Iteration {
-    public static int fact(int n) {
-        if (n == 1) {
-            return 1;
-        }
-        int f = n + fact(n - 1);
-        return f;
-    }
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.println("Factorial By using function: " + fact(n));
-        int f = 0;
-        for (int i = 1; i <= n; i++) {
-            f = f + i;
+        int sum = 0;
+        while (n >= 1) {
+            sum = sum + n;
+            n--;
         }
-        System.out.println("Fctorials by using the Iteration: " + f);
+        System.out.println(sum);
     }
 }
