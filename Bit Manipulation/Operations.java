720512a98a8218c15488ae79c153bc16a266d8b2
Vishal <vishalpal1501@gmail.com>
2024-06-16 12:02:04 +0530

    get Ith bit

---
+++ Bit Manipulation/Operations.java
@@ -0,0 +1,15 @@
+
+public class Operations {
+    public static int getithBit(int n, int i) {
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println(getithBit(10, 3));
+    }
+}
