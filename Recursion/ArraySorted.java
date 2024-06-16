0e40fdcad0020e86926161afd780025a75b8da1b
Vishal <vishalpal1501@gmail.com>
2024-06-16 13:35:05 +0530

    WAF to find  first occurence of an element in an array

--- Recursion/ArraySorted.java
+++ Recursion/ArraySorted.java
@@ -11,9 +11,21 @@ public class ArraySorted {
         return isSorted(arr, i + 1);
     }
 
+    public static int firstOccurrence(int arr[], int key, int i) {
+        if (i == arr.length - 1) {
+            return -1;
+        }
+        if (key == arr[i]) {
+            return i;
+        }
+
+        return firstOccurrence(arr, key, i + 1);
+    }
+
     public static void main(String args[]) {
         int arr[] = { 1, 2, 3, 9, 5 };
         System.out.println(isSorted(arr, 0));
+        System.out.println(firstOccurrence(arr, 9, 0));
 
     }
 }
